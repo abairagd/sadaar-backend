887dc2611761953836de36acd5a6d7e0cfac7838
abairagd <abairagd@gmail.com>
2026-07-27 15:40:07 +0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const requireBrandAuth = require("../middleware/requireBrandAuth");
 const upload = require("../middleware/upload");
-const { listProducts, getProduct, createProduct } = require("../controllers/productsController");
+const { listProducts, getProduct, createProduct, updateVariantStock, archiveProduct } = require("../controllers/productsController");
 const { uploadProductImage, deleteProductImage, reorderProductImages } = require("../controllers/imagesController");
 
 router.get("/", listProducts);
 router.get("/:id", getProduct);
 router.post("/", requireBrandAuth, createProduct);
+router.patch("/:id/variants/:variantId/stock", requireBrandAuth, updateVariantStock);
+router.delete("/:id", requireBrandAuth, archiveProduct);
 router.post("/:id/images", requireBrandAuth, upload.single("image"), uploadProductImage);
 router.delete("/:id/images/:imageId", requireBrandAuth, deleteProductImage);
 router.patch("/:id/images/reorder", requireBrandAuth, reorderProductImages);
