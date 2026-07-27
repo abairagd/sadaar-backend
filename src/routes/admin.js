e0c88b0327c9fb06fa1d0c445ee9a137de3b74e3
abairagd <abairagd@gmail.com>
2026-07-27 13:06:42 +0300

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { adminLogin, requireAdminAuth } = require("../middleware/adminAuth");
-const { listAllBrands, updateBrandStatus, updateBrandCommission, platformStats, listPendingPayouts, markBrandPayoutsPaid } = require("../controllers/adminController");
+const { listAllBrands, updateBrandStatus, updateBrandCommission, platformStats, listPendingPayouts, markBrandPayoutsPaid, listAllOrders, listAllOrderItems } = require("../controllers/adminController");
 const { authLimiter } = require("../middleware/rateLimiters");
 
 router.post("/login", authLimiter, adminLogin);
@@ -11,5 +11,7 @@ router.patch("/brands/:id/commission", requireAdminAuth, updateBrandCommission);
 router.get("/stats", requireAdminAuth, platformStats);
 router.get("/payouts", requireAdminAuth, listPendingPayouts);
 router.post("/payouts/:brandId/mark-paid", requireAdminAuth, markBrandPayoutsPaid);
+router.get("/orders", requireAdminAuth, listAllOrders);
+router.get("/order-items", requireAdminAuth, listAllOrderItems);
 
 module.exports = router;
