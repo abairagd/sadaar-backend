6118b2624950570ac72af3a072dc6d8ec11b4360
abairagd <abairagd@gmail.com>
2026-07-28 15:32:08 +0300

    Update brands.js

--- src/routes/brands.js
+++ src/routes/brands.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { listBrands, getBrand, applyBrand, loginBrand, requestPasswordReset, resetPassword } = require("../controllers/brandsController");
+const requireBrandAuth = require("../middleware/requireBrandAuth");
+const { listBrands, getBrand, applyBrand, loginBrand, requestPasswordReset, resetPassword, getMyBrandProfile, updateMyBrandProfile } = require("../controllers/brandsController");
 const { authLimiter, writeLimiter } = require("../middleware/rateLimiters");
 
 router.get("/", listBrands);
+router.get("/me", requireBrandAuth, getMyBrandProfile);
+router.patch("/me", requireBrandAuth, writeLimiter, updateMyBrandProfile);
 router.get("/:slug", getBrand);
 router.post("/apply", writeLimiter, applyBrand);
 router.post("/login", authLimiter, loginBrand);
