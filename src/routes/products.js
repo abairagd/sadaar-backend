534b8979cc0de04458315a88e17277520dd3fd59
abairagd <abairagd@gmail.com>
2026-07-28 15:32:31 +0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const requireBrandAuth = require("../middleware/requireBrandAuth");
 const upload = require("../middleware/upload");
-const { listProducts, getProduct, createProduct, updateProduct, updateVariantStock, archiveProduct } = require("../controllers/productsController");
+const { listProducts, getProduct, createProduct, updateProduct, updateVariantStock, archiveProduct, toggleSignatureProduct } = require("../controllers/productsController");
 const { uploadProductImage, deleteProductImage, reorderProductImages } = require("../controllers/imagesController");
 
 router.get("/", listProducts);
 router.get("/:id", getProduct);
-router.post("/", requireBrandAuth, createProduct);
+router.post("/", requireBrandAuth, createProduct); // brand dashboard adds a product to their own catalog
 router.patch("/:id", requireBrandAuth, updateProduct);
+router.patch("/:id/signature", requireBrandAuth, toggleSignatureProduct);
 router.patch("/:id/variants/:variantId/stock", requireBrandAuth, updateVariantStock);
 router.delete("/:id", requireBrandAuth, archiveProduct);
 router.post("/:id/images", requireBrandAuth, upload.single("image"), uploadProductImage);
