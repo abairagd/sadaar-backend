208b3d002542054342b196d18656660a3e217163
abairagd <abairagd@gmail.com>
2026-08-10 13:24:46 +0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const requireBrandAuth = require("../middleware/requireBrandAuth");
 const upload = require("../middleware/upload");
-const { listProducts, getProduct, createProduct, updateProduct, updateVariantStock, archiveProduct, toggleSignatureProduct } = require("../controllers/productsController");
+const { listProducts, getProduct, createProduct, bulkCreateProducts, updateProduct, updateVariantStock, archiveProduct, toggleSignatureProduct } = require("../controllers/productsController");
 const { uploadProductImage, deleteProductImage, reorderProductImages } = require("../controllers/imagesController");
 
 router.get("/", listProducts);
@@ -13,6 +13,7 @@ router.patch("/:id/signature", requireBrandAuth, toggleSignatureProduct);
 router.patch("/:id/variants/:variantId/stock", requireBrandAuth, updateVariantStock);
 router.delete("/:id", requireBrandAuth, archiveProduct);
 router.post("/:id/images", requireBrandAuth, upload.single("image"), uploadProductImage);
+router.post("/bulk", requireBrandAuth, bulkCreateProducts);
 router.delete("/:id/images/:imageId", requireBrandAuth, deleteProductImage);
 router.patch("/:id/images/reorder", requireBrandAuth, reorderProductImages);
 
