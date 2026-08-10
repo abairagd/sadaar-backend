7f33ce0229ba7f4dd2a9e9db4ca000d6a5f0a937
Assets-group <ahmed@assets-group.com>
2026-08-10 16:29:52 +0300

    Add brand vetting checklist enforcement

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { adminLogin, requireAdminAuth } = require("../middleware/adminAuth");
-const { listAllBrands, updateBrandStatus, updateBrandCommission, platformStats, listPendingPayouts, markBrandPayoutsPaid, listAllOrders, listAllOrderItems } = require("../controllers/adminController");
+const { listAllBrands, updateBrandStatus, updateBrandVetting, updateBrandCommission, platformStats, listPendingPayouts, markBrandPayoutsPaid, listAllOrders, listAllOrderItems } = require("../controllers/adminController");
 const { authLimiter } = require("../middleware/rateLimiters");
 
 router.post("/login", authLimiter, adminLogin);
 router.get("/brands", requireAdminAuth, listAllBrands);
 router.patch("/brands/:id/status", requireAdminAuth, updateBrandStatus);
+router.patch("/brands/:id/vetting", updateBrandVetting);
 router.patch("/brands/:id/commission", requireAdminAuth, updateBrandCommission);
 router.get("/stats", requireAdminAuth, platformStats);
 router.get("/payouts", requireAdminAuth, listPendingPayouts);
